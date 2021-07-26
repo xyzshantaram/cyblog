e81fbe1220806ae02e9c9f8a9c241cdba2389c1a
Siddharth Singh <simsid66@gmail.com>
2021-07-27 00:02:43 +0530

    Add utils.ts

--- README.md
+++ README.md
@@ -52,3 +52,8 @@ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ```
+
+### Acknowledgements
+
+`getConfigDir()` taken from https://github.com/justjavac/deno_data_dir under the
+MIT license. Copyright © justjavac.
---
+++ utils.ts
@@ -0,0 +1,48 @@
+export type Path = string | URL;
+
+export enum PathTypes {
+    File = 0,
+    Directory
+}
+
+export interface CyblogBuildArgs {
+    to?: Path;
+    applyStyles?: Path | Path[];
+}
+
+export function scream(code: number, ...data: unknown[]) {
+    if (code == 0) console.log(...data);
+    else console.error(...data);
+    Deno.exit(code);
+}
+
+export function getFileName(name: string, type: string) {
+    return name.slice(undefined, name.length - type.length);
+}
+
+export async function getType(path: Path) {
+    if ((await Deno.stat(path)).isFile) return PathTypes.File;
+    return PathTypes.Directory;
+}
+
+export function getConfigDir(): string | null {
+    switch (Deno.build.os) {
+        case "linux": {
+            const xdg = Deno.env.get("XDG_DATA_HOME");
+            if (xdg) return xdg;
+
+            const home = Deno.env.get("HOME");
+            if (home) return `${home}/.local/share`;
+            break;
+        }
+        case "darwin": {
+            const home = Deno.env.get("HOME");
+            if (home) return `${home}/Library/Application Support`;
+            break;
+        }
+        case "windows":
+            return Deno.env.get("FOLDERID_RoamingAppData") ?? null;
+    }
+
+    return null;
+}
