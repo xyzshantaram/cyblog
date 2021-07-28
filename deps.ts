9a7544932848f5cee6802f60d2a69094d0f959f4
Siddharth Singh <simsid66@gmail.com>
2021-07-29 01:56:59 +0530

    Fix deps

--- deps.ts
+++ deps.ts
@@ -1,1 +1,3 @@
 export * as flags from "https://deno.land/std@0.102.0/flags/mod.ts";
+export * as path from "https://deno.land/std@0.102.0/path/mod.ts";
+export * as fs from "https://deno.land/std@0.102.0/fs/mod.ts";
