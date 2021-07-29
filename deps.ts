4c3490665cd2c9bb4b161d15fd606da1a16e2439
Siddharth Singh <simsid66@gmail.com>
2021-07-29 11:07:18 +0530

    Import Marked markdown compiler

--- deps.ts
+++ deps.ts
@@ -1,3 +1,4 @@
 export * as flags from "https://deno.land/std@0.102.0/flags/mod.ts";
 export * as path from "https://deno.land/std@0.102.0/path/mod.ts";
 export * as fs from "https://deno.land/std@0.102.0/fs/mod.ts";
+export { Marked } from "https://deno.land/x/markdown@v2.0.0/mod.ts";
