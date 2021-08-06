1da9491d48fa074b1e4e16b2de3000525b68c5f6
Siddharth Singh <simsid66@gmail.com>
2021-08-07 00:28:00 +0530

    Improve config dir detection, use CYBLOG_DATA_DIR env variable

--- src/utils.ts
+++ src/utils.ts
@@ -42,25 +42,46 @@ export async function getType(path: Path) {
 }
 
 export function getConfigDir(): string | null {
+    let rval: string | null = null;
+    
+    rval = Deno.env.get("CYBLOG_DATA_DIR") || null; // respect CYBLOG_DATA_DIR env setting
+    // to override default cyblog data path
+
     switch (Deno.build.os) {
         case "linux": {
+            if (rval) break;
+            // look for XDG_DATA_HOME, if not found, default to $HOME/.local/share
             const xdg = Deno.env.get("XDG_DATA_HOME");
-            if (xdg) return xdg;
+            let fallback: string | undefined | null = Deno.env.get("HOME");
+            fallback = fallback ? `${fallback}/.local/share` : null;
 
-            const home = Deno.env.get("HOME");
-            if (home) return `${home}/.local/share`;
+            rval = xdg || fallback || null;
             break;
         }
         case "darwin": {
-            const home = Deno.env.get("HOME");
-            if (home) return `${home}/Library/Application Support`;
+            if (rval) break;
+            // Look for $HOME, return $HOME/Library/Application Support
+            // if it exists
+            let home: string | null | undefined = Deno.env.get("HOME");
+            home = home ? `${home}/Library/Application Support` : null;
+
+            rval = home;
             break;
         }
-        case "windows":
-            return Deno.env.get("FOLDERID_RoamingAppData") ?? null;
+        case "windows": {
+            if (rval) break;
+            // Look for appdata, use it if it is set
+            rval = Deno.env.get("APPDATA") || null;
+            break;
+        }
+        default: {
+            // fall back to CYBLOG_DATA_DIR again if we don't know what OS this is.
+            rval = Deno.env.get("CYBLOG_DATA_DIR") || null;
+            break;
+        }
     }
 
-    return null;
+    return rval;
 }
 
 export function createElementWithAttrs(name: string, args: Record<string, unknown>) {
