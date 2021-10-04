df8cdb62d57e134539dac24132c10f0de7071edb
Siddharth Singh <simsid66@gmail.com>
2021-10-05 01:46:24 +0530

    bump macrolight to v1.3.1

--- src/deps.ts
+++ src/deps.ts
@@ -2,4 +2,4 @@ export * as flags from "https://deno.land/std@0.102.0/flags/mod.ts";
 export * as path from "https://deno.land/std@0.102.0/path/mod.ts";
 export * as fs from "https://deno.land/std@0.102.0/fs/mod.ts";
 export { Marked, Renderer } from "https://deno.land/x/markdown@v2.0.0/mod.ts";
-export { highlight } from 'https://esm.sh/macrolight@1.3.0';
+export { highlight } from 'https://esm.sh/macrolight@1.3.1';
