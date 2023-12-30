a66f149156b6e6fea10793aed634e7ff91e50f82
Siddharth Singh <me@shantaram.xyz>
2023-12-30 18:06:40 +0530

    delete cyblog files

--- docs/build.ts
+++
@@ -1,9 +0,0 @@
-import { bundle } from "./src/deps.ts";
-
-const result = await bundle("./src/main.ts", {
-    compilerOptions: {
-        checkJs: true
-    }
-});
-
-await Deno.writeTextFile('cyblog.js', result.code);
